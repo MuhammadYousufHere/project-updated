0f9be79faf4a05d0c762bf330c8e91add14fa307
MuhammadYousuf20 <39589913+MuhammadYousufHere@users.noreply.github.com>
2018-07-30 14:26:45 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 function getTable()	{
- let number = document.querySelector(".initial").value;
- let start = document.querySelector(".start").value;
- let end = document.querySelector(".end").value;
+
+ let number = parseInt(document.querySelector(".initial").value);
+ let start = parseInt(document.querySelector(".start").value);
+ let end = parseInt(document.querySelector(".end").value);
  let output = document.querySelector("section");
  for (let i = start;  i <= end; i++) { 
- 	if (end <=15) {
- 	console.log(number +" x "+ i +" = " + i * number); 
- } else {
- 	console.log("Please follow the limit.")
- }
-
+ 	var node = document.createElement("div");
+    var textnode = document.createTextNode(number +" x "+ i +" = " + i * number);
+    node.appendChild(textnode);
+    output.appendChild(node);
+   
 }
 }
